0364887d72cc1be1b75104a30a7b3103f0150e51
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-26 22:53:25 -0300

    Criando a primeira view

--- js/app.js
+++ js/app.js
@@ -6,5 +6,15 @@
 */
 
 const app = new Vue({
-    el: '#app'
+    el: '#app',
+    data: {
+        pokemonList: [
+            {name: 'Charizard', number:'006'},
+            {name: 'Squirtle', number:'007'},
+            {name: 'Pikachu', number:'025'},
+            {name: 'Celebi', number:'251'},
+            {name: 'Lucario', number:'448'},
+            {name: 'Delphox', number:'655'}
+        ]
+    }
 })
