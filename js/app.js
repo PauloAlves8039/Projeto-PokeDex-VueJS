86946e8b28cff66b1a4c0219b8edaa9d43597d2e
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-05 22:07:07 -0300

    Lógica para consumo de dados do servidor

--- js/app.js
+++ js/app.js
@@ -8,16 +8,14 @@
 const app = new Vue({
     el: '#app',
     data: {
-        pokemonList: [
-            {name: 'charizard', number: 6},
-            {name: 'squirtle', number: 7},
-            {name: 'pikachu', number: 25},
-            {name: 'celebi', number: 251},
-            {name: 'lucario', number: 448},
-            {name: 'delphox', number: 655}
-        ],
+        pokemonList: [],
         nameFilter: ''
     },
+    mounted: function(){
+        PokeService.API.Pokemon.listAll().then(pokemonList => {
+            this.pokemonList = pokemonList
+        })
+      },
     computed: {
         pokeList: function(){
             let nameFilter = this.nameFilter.toLowerCase()
---
+++ js/services.js
@@ -0,0 +1,33 @@
+/*
+ * Arquivo: services.js
+ * Autor: Paulo Alves
+ * Descrição: responsável pela criação de requisições no servidor 
+ * Data: 05/02/2020
+*/
+
+let PokeService = {
+    API: {
+        url: 'https://dev.treinaweb.com.br/pokeapi/pokedex/1',
+        get: (url) => {
+            return fetch(`${PokeService.API.url}${url}`)
+                .then(response => response.json())
+        },
+        Pokemon: {
+            listAll: () => {
+                return PokeService.API.get('pokedex/1')
+                    .then(response => {
+                        return response.pokemon
+                            .map(pokemon => {
+                                pokemon.number = PokeService.getNumberFromURL(pokemon.resource_uri)
+                                return pokemon
+                            })
+                            .filter(pokemon => pokemon.number < 1000)
+                            .sort((a, b) => (a.number > b.number ? 1 : -1))
+                    })
+            }
+        }
+    },
+    getNumberFromURL: (url) => {
+        return parseInt(url.replace(/.*\/(\d+)\/$/,'$1'))
+    }
+}
