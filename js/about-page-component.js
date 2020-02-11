bb644617796d5cf7bc090154daab2122096a1c21
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-11 11:05:16 -0300

    Ajustando componentes

--- js/about-page-component.js
+++ js/about-page-component.js
@@ -8,17 +8,20 @@
 Vue.component('about-page', {
     template: `   
         <div>
-            <router-link to="/" class="back-button"> &lt; </router-link> 
-            <div class="poke-profile">
-                <div>#{{ $route.params.number }} - {{ pkm.name }}</div>
-                <img class="poke-sprite" :src="'//serebii.net/sunmoon/pokemon/'+$route.params.number+'.png'" />
+            <router-link to="/"  class="back-button"  > &lt; </router-link>
+        
+            <div class="poke-profile" >
+               <div>#{{ $route.params.number }} - {{pkm.name}}</div>
+               <img class="poke-sprite" :src="'//serebii.net/sunmoon/pokemon/'+$route.params.number+'.png'" />
             </div>
-            <ul class="poke-types">
-                <li v-for="type in pkm.types">
+            
+            <ul class="poke-types" >
+                <li v-for="type in pkm.types" >
                     <img :src="'//serebii.net/pokedex-bw/type/'+type.name+'.gif'" />
-                </li>
+                </li>   
             </ul>
-            <table class="stats">
+            
+            <table class="stats" >
                 <tr>
                     <td>Attack</td>
                     <td>Defense</td>
@@ -34,6 +37,7 @@ Vue.component('about-page', {
                     <td>{{ pkm.speed }}</td>
                 </tr>
             </table>
+            
         </div>
     `,
     data: function(){
--- js/home-page-component.js
+++ js/home-page-component.js
@@ -8,9 +8,10 @@
 Vue.component('home-page', {
     template: `
         <div>
-            <input v-model="$parent.nameFilter" type="text" id="pokeFilter" placeholder="Search" maxlength="20">
-            <ul class="poke-list">
-                <my-list v-for="item in $parent.pokeList" :pokemon="item" :key="item.number"></my-list>
+            <input v-model="$parent.nameFilter" type="text" id="pokeFilter" placeholder="Search" maxlength="20" />
+    
+            <ul class="poke-list" >
+                <my-list v-for="item in $parent.pokeList" :pokemon="item" :key="item.number" ></my-list>
             </ul>
         </div>
     `
