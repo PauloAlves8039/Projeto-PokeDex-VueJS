d4e35de03589ab620235eb8fb975919625184f5f
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-10 22:16:44 -0300

    Implementado template para a home page

--- js/home-page-component.js
+++ js/home-page-component.js
@@ -5,3 +5,14 @@
  * Data: 10/02/2020
 */
 
+Vue.component('home-page', {
+    template: `
+        <div>
+            <input v-model="$parent.nameFilter" type="text" id="pokeFilter" placeholder="Search" maxlength="20">
+            <ul class="poke-list">
+                <my-list v-for="item in $parent.pokeList" :pokemon="item" :key="item.number"></my-list>
+            </ul>
+        </div>
+    `
+})
+
